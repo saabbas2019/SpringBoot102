915616a7825a412113df21dea626cfa581981533
GBTC405007UR <GBTC405007UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-19 14:29:25 -0400

    Includes questionn answers

---
+++ src/main/java/com/example/demo/SpringBoor102Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoor102Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoor102Application.class, args);
+    }
+
+}
